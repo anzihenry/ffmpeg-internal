e34d2d6f76716080b9a5ff6bd66ec56784e2dd44
Henry Tse <anzihenry@gmail.com>
2022-05-31 02:15:02 +0800

    增加默认文件，配置信息

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,10 +1,16 @@
 # Configuration file for the Sphinx documentation builder.
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+
+import sphinx_rtd_theme
+
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'ffmpeg internal'
+copyright = '2022, Henry Tse'
+author = 'Henry Tse'
 
 release = '0.1'
 version = '0.1.0'
@@ -30,6 +36,7 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
